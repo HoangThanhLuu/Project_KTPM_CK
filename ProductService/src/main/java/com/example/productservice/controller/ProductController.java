2e9454d754fef946f6f6f014c979bdad9febfd4d
Dvwnmark <xcmark141@gmail.com>
2024-04-20 11:00:08 +0700

    update application gateway , port mapping product, user

--- ProductService/src/main/java/com/example/productservice/controller/ProductController.java
+++ ProductService/src/main/java/com/example/productservice/controller/ProductController.java
@@ -16,7 +16,7 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
-@RequestMapping("/api/v1")
+@RequestMapping("/api/v2")
 public class ProductController {
     @Autowired
     ProductService productService;
--- UserService/src/main/java/com/iuh/user/controller/UserController.java
+++ UserService/src/main/java/com/iuh/user/controller/UserController.java
@@ -11,7 +11,7 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
-@RequestMapping("/api/v2")
+@RequestMapping("/api/v1")
 public class UserController {
     @Autowired
     UserService userService;
